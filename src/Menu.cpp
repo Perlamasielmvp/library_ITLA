fb9eb153d2586cee1676ace58c450710cb73560b
Perlamasielmvp <20142210@itla.edu.do>
2016-06-20 20:46:35 -0400

    updating solution

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -21,3 +21,123 @@ Menu::Menu()
 
 }
 
+class book
+{
+	char bno[6];
+	char bname[50];
+	char aname[20];
+  public:
+	void create_book()
+	{
+		cout<<"\nNEW BOOK ENTRY...\n";
+		cout<<"\nEnter The book no.";
+		cin>>bno;
+		cout<<"\n\nEnter The Name of The Book ";
+		gets(bname);
+		cout<<"\n\nEnter The Author's Name ";
+		gets(aname);
+		cout<<"\n\n\nBook Created..";
+	}
+
+	void show_book()
+	{
+		cout<<"\nBook no. : "<<bno;
+		cout<<"\nBook Name : ";
+		puts(bname);
+		cout<<"Author Name : ";
+		puts(aname);
+	}
+
+	void modify_book()
+	{
+		cout<<"\nBook no. : "<<bno;
+		cout<<"\nModify Book Name : ";
+		gets(bname);
+		cout<<"\nModify Author's Name of Book : ";
+		gets(aname);
+	}
+
+	char* retbno()
+	{
+		return bno;
+	}
+
+	void report()
+	{cout<<bno<<setw(30)<<bname<<setw(30)<<aname<<endl;}
+
+
+};         //class ends here
+
+
+
+
+class student
+{
+	char admno[6];
+	char name[20];
+	char stbno[6];
+	int token;
+public:
+	void create_student()
+	{
+		clrscr();
+	 	cout<<"\nNEW STUDENT ENTRY...\n";
+		cout<<"\nEnter The admission no. ";
+		cin>>admno;
+		cout<<"\n\nEnter The Name of The Student ";
+		gets(name);
+		token=0;
+		stbno[0]='/0';
+		cout<<"\n\nStudent Record Created..";
+	}
+
+	void show_student()
+	{
+		cout<<"\nAdmission no. : "<<admno;
+		cout<<"\nStudent Name : ";
+		puts(name);
+		cout<<"\nNo of Book issued : "<<token;
+		if(token==1)
+			cout<<"\nBook No "<<stbno;
+	}
+
+	void modify_student()
+	{
+		cout<<"\nAdmission no. : "<<admno;
+		cout<<"\nModify Student Name : ";
+		gets(name);
+	}
+
+	char* retadmno()
+	{
+		return admno;
+	}
+
+	char* retstbno()
+	{
+		return stbno;
+	}
+
+	int rettoken()
+	{
+		return token;
+	}
+
+	void addtoken()
+	{token=1;}
+
+	void resettoken()
+	{token=0;}
+
+	void getstbno(char t[])
+	{
+		strcpy(stbno,t);
+	}
+
+	void report()
+	{cout<<"\t"<<admno<<setw(20)<<name<<setw(10)<<token<<endl;}
+
+};         //class ends here
+
+
+
--- src/Stack.cpp
+++ src/Stack.cpp
@@ -23,3 +23,31 @@ Stack::Stack(): _last(NULL)
 
 }
 
+void write_book()
+{
+	char ch;
+	fp.open("book.dat",ios::out|ios::app);
+	do
+	{
+		clrscr();
+		bk.create_book();
+		fp.write((char*)&bk,sizeof(book));
+		cout<<"\n\nDo you want to add more record..(y/n?)";
+		cin>>ch;
+	}while(ch=='y'||ch=='Y');
+	fp.close();
+}
+
+void write_student()
+{
+	char ch;
+	fp.open("student.dat",ios::out|ios::app);
+	do
+	{
+		st.create_student();
+		fp.write((char*)&st,sizeof(student));
+		cout<<"\n\ndo you want to add more record..(y/n?)";
+		cin>>ch;
+	}while(ch=='y'||ch=='Y');
+	fp.close();
+}
